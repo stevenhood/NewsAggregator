80300b24f7d8bde0837b0843bb76725152a720e6
stevenhood <steven.hood@live.co.uk>
2016-12-18 11:16:27 +0000

    Added Swagger, set up simple call for BBC news JSON
    Set up Swagger config to use C# XML comments. Removed Home controller.

---
+++ NewsAggregatorAPI/Controllers/NewsController.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Xml;
+
+namespace NewsAggregatorAPI.Controllers
+{
+    /// <summary>
+    /// The controller for retrieving news.
+    /// </summary>
+    public class NewsController : ApiController
+    {
+        /// <summary>
+        /// Get news feed by provider name.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("news/{providerName}")]
+        public async Task<IHttpActionResult> GetFeedByName(string providerName)
+        {
+            using (var client = new HttpClient())
+            {
+                var responseXml = await client.GetStringAsync(@"http://feeds.bbci.co.uk/news/rss.xml");
+
+                var doc = new XmlDocument();
+                doc.LoadXml(responseXml);
+                var jsonText = JsonConvert.SerializeXmlNode(doc);
+
+                return Ok(jsonText);
+            }
+        }
+    }
+}
